1d5d42e34c5e01af001f23abbc3c39568ff3bce5
Tim <timcarlsson0203@gmail.com>
2024-11-27 21:27:42 +0100

    added circular resizable queue implementation, still needs testing though

--- src/queue.c
+++ src/queue.c
@@ -1,5 +1,116 @@
 #include "queue.h"
 
-void enqueue(queue *s, int);
-int dequeue(queue *s);
-int peek(queue *s);
+
+
+queue *create_queue(int initial_capacity)
+{
+    queue *q = malloc(sizeof(queue));
+    if (!q)
+    {
+        return NULL;
+    }
+    q->data = malloc(initial_capacity * sizeof(int));
+
+    if (!q->data) 
+    {
+        fprintf(stderr, "memory allocation failed\n");
+        return NULL;
+    }
+
+    q->capacity = initial_capacity;
+    q->size = 0;
+    q->front = -1;
+    q->back = -1;
+    
+    return q;
+}
+
+
+void resize_queue(queue *q)
+{
+    // Creates a new array and copies the current array to it starting from the head and looping around if neccassary
+    int new_capacity = q->capacity * 2;
+
+    int *new_data = malloc(new_capacity * sizeof(int)); // Allocate the space for the new queue contents
+
+    for (int i = 0; i < q->size; i++)
+    {
+        new_data[i] = q->data[(q->front + i) % q->capacity];
+    }
+    free(q->data);
+    q->data = new_data;
+    q->capacity = new_capacity;
+    q->front = 0;
+    q->back = q->size;
+}
+
+
+void enqueue(queue *q, int element)
+{
+    assert(q);
+
+    if (q->front == -1 && q->back == -1)
+    {
+        // The queue is empty 
+        q->front = 0;
+        q->back = 0;
+
+    }
+    else if (q->front == q->back)
+    {
+        resize_queue(q);
+    }
+
+    q->data[q->back] = element;
+    q->back++;
+    q->size++;
+}
+
+
+int dequeue(queue *q)
+{
+    if (queue_is_empty(q))
+    {
+        fprintf(stderr, "Queue is empty can't dequeue.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int result = q->data[q->front];
+    q->front++;
+    q->size--;
+
+    if (q->front == q->back)
+    {
+        q->front = -1;
+        q->back = -1;
+    }
+
+    return result; 
+}
+
+
+int peek(queue *q)
+{
+    assert(q);
+
+    if (queue_is_empty(q))
+    {
+        fprintf(stderr, "Can't peek, queue is empty.\n");
+        exit(EXIT_FAILURE);
+    }
+    return q->data[q->front];
+}
+
+
+int queue_size(queue *q)
+{
+    assert(q);
+
+    return q->size;
+}
+
+
+bool queue_is_empty(queue *q)
+{
+    return (q->front -1 && q->back == -1);
+}
--- src/queue.h
+++ src/queue.h
@@ -3,20 +3,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h>
 #include <assert.h>
+#include <stdbool.h>
 
-typedef struct 
+typedef struct
 {
     int *data;
     int capacity;
     int size;
+    int front;
+    int back;
 } queue;
 
-queue *create_queue(size_t initial_capacity);
-void stack_resize(queue *s);
-void enqueue(queue *s, int);
-int dequeue(queue *s);
-int peek(queue *s);
-int queue_size(queue *s);
-bool queue_is_empty(queue *s);
+queue *create_queue(int initial_capacity);
+void resize_queue(queue *q);
+void enqueue(queue *q, int);
+int dequeue(queue *q);
+int peek(queue *q);
+int queue_size(queue *q);
+bool queue_is_empty(queue *q);
